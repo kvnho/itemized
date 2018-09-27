1e7b1b05ad7df419ad2e8627f331638251bd2fce
Kevin Ho <kevinho479@gmail.com>
2018-09-26 21:51:57 -0400

    first commit

---
+++ src/main/java/com/kevinho/itemized/ItemizedApplication.java
@@ -0,0 +1,12 @@
+package com.kevinho.itemized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemizedApplication.class, args);
+	}
+}
